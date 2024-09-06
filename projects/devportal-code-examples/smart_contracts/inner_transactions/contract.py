89e689e9802aaab52d77693688203bfa5f6f4213
Chris Kim (Hyunggun) <chris.kim@algorand.foundation>
2024-09-06 11:40:24 -0700

    revert

--- projects/devportal-code-examples/smart_contracts/inner_transactions/contract.py
+++ projects/devportal-code-examples/smart_contracts/inner_transactions/contract.py
@@ -3,13 +3,13 @@ from algopy.arc4 import abimethod
 
 
 class InnerTransactions(ARC4Contract):
-    # example-payment
+    # EXAMPLE:payment
     @abimethod()
-    def payment(self) -> Bytes:
+    def payment(self) -> UInt64:
         result = itxn.Payment(amount=5000, receiver=Txn.sender, fee=0).submit()
-        return result.txn_id
+        return result.amount
     
     # fee is set to 0 by default. Manually set here for demonstration purposes.
     # The `Sender` for the above is implied to be Global.current_application_address().
     # If a different sender is needed, it'd have to be an account that has been rekeyed to the application address. 
-    # example-payment
+    # EXAMPLE:payment
