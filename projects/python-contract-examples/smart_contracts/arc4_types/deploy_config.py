1457811ccc8f0af6076e4f3bf22e5dadc557a951
Chris Kim (Hyunggun) <chris.kim@algorand.foundation>
2024-10-25 14:56:31 -0700

    initial commit to set up arc4 types examples

---
+++ projects/python-contract-examples/smart_contracts/arc4_types/deploy_config.py
@@ -0,0 +1,36 @@
+import logging
+
+import algokit_utils
+from algosdk.v2client.algod import AlgodClient
+from algosdk.v2client.indexer import IndexerClient
+from smart_contracts.artifacts.arc4_types.arc4_types_client import (
+        Arc4TypesClient,
+    )
+
+logger = logging.getLogger(__name__)
+
+
+# define deployment behaviour based on supplied app spec
+def deploy(
+    algod_client: AlgodClient,
+    indexer_client: IndexerClient,
+    app_spec: algokit_utils.ApplicationSpecification,
+    deployer: algokit_utils.Account,
+) -> None:
+
+
+    app_client = Arc4TypesClient(
+        algod_client,
+        creator=deployer,
+        indexer_client=indexer_client,
+    )
+    app_client.deploy(
+        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
+        on_update=algokit_utils.OnUpdate.AppendApp,
+    )
+    name = "world"
+    response = app_client.hello(name=name)
+    logger.info(
+        f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
+        f"with name={name}, received: {response.return_value}"
+    )
