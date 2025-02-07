647eb4afbe2d60f8deb578031cc9c352d1dec3d4
Chris Kim (Hyunggun) <chris.kim@algorand.foundation>
2025-02-07 15:12:20 -0800

    upgrade hello world test to utils v3

--- projects/python-examples/tests/hello_world_integration_test.py
+++ projects/python-examples/tests/hello_world_integration_test.py
@@ -1,48 +1,61 @@
-import algokit_utils
 import pytest
-from algokit_utils import get_localnet_default_account
+from algokit_utils import *
 from algokit_utils.config import config
-from algosdk.v2client.algod import AlgodClient
-from algosdk.v2client.indexer import IndexerClient
 
-from smart_contracts.artifacts.hello_world.hello_world_client import HelloWorldClient
+from smart_contracts.artifacts.hello_world.hello_world_client import (
+    HelloArgs,
+    HelloWorldClient,
+    HelloWorldFactory,
+)
 
 
 @pytest.fixture(scope="session")
 def hello_world_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
+    algorand: AlgorandClient, dispenser: SigningAccount, creator: SigningAccount
 ) -> HelloWorldClient:
     config.configure(
         debug=True,
         # trace_all=True,
     )
 
-    client = HelloWorldClient(
-        algod_client,
-        creator=get_localnet_default_account(algod_client),
-        indexer_client=indexer_client,
+    factory = algorand.client.get_typed_app_factory(
+        HelloWorldFactory,
+        default_sender=creator.address,
+        default_signer=creator.signer,
     )
 
-    client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
+    client, deploy_result = factory.deploy(
+        on_update=OnUpdate.ReplaceApp,
+        on_schema_break=OnSchemaBreak.Fail,
     )
+
+    algorand.send.payment(
+        PaymentParams(
+            sender=dispenser.address,
+            receiver=client.app_address,
+            amount=AlgoAmount(algo=1),  # 1 Algo
+        )
+    )
+
     return client
 
 
 def test_says_hello(hello_world_client: HelloWorldClient) -> None:
-    result = hello_world_client.hello(name="World")
+    result = hello_world_client.send.hello(HelloArgs(name="World"))
 
     assert result.abi_return == "Hello, World"
 
 
 def test_simulate_says_hello_with_correct_budget_consumed(
-    hello_world_client: HelloWorldClient, algod_client: AlgodClient
+    hello_world_client: HelloWorldClient, algorand: AlgorandClient
 ) -> None:
     result = (
-        hello_world_client.compose().hello(name="World").hello(name="Jane").simulate()
+        hello_world_client.new_group()
+        .hello(HelloArgs(name="World"))
+        .hello(HelloArgs(name="Jane"))
+        .simulate()
     )
 
-    assert result.abi_results[0].abi_return == "Hello, World"
-    assert result.abi_results[1].abi_return == "Hello, Jane"
+    assert result.returns[0].value == "Hello, World"
+    assert result.returns[1].value == "Hello, Jane"
     assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
