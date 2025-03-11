1e7e1e6a6e5ea113429fd1a2b0915de90c3e0fda
Chris Kim (Hyunggun) <chris.kim@algorand.foundation>
2025-03-11 00:14:44 -0700

    feat: fee pooling example

--- projects/python-examples/algokit_utils_py_examples/transactions/fees.py
+++ projects/python-examples/algokit_utils_py_examples/transactions/fees.py
@@ -1,10 +1,12 @@
 import json
 
+from algokit_utils import AlgoAmount, PaymentParams
+
 from algokit_utils_py_examples.helpers import setup_localnet_environment
 
 
 def fees() -> None:
-    algorand_client, _, _, _, _ = setup_localnet_environment()
+    algorand_client, _, account_a, account_b, _ = setup_localnet_environment()
 
     # example: FEES
 
@@ -45,6 +47,63 @@ def fees() -> None:
         if not attr.startswith("_") and not callable(getattr(sp, attr))
     }
     print(json.dumps(sp_dict, indent=2))
+
+    # example: FEE_POOLING
+
+    sp = algorand_client.get_suggested_params()
+
+    transaction_a = algorand_client.create_transaction.payment(
+        PaymentParams(
+            sender=account_a.address,
+            receiver=account_b.address,
+            amount=AlgoAmount(algo=2),
+            static_fee=AlgoAmount(algo=0),  # fee is set to 0
+        )
+    )
+
+    transaction_b = algorand_client.create_transaction.payment(
+        PaymentParams(
+            sender=account_a.address,
+            receiver=account_b.address,
+            amount=AlgoAmount(algo=1),
+            extra_fee=max(
+                sp.fee * transaction_a.estimate_size(),
+                AlgoAmount(micro_algo=sp.min_fee),
+            ),  # extra fee is set to the max of the suggested fee or the minimum fee to cover the fee of transaction_a
+        )
+    )
+
+    """
+    transaction_a and transaction_b are added to the same atomic group.
+    The extra fee of transaction_b covers the fee of transaction_a.
+    Therefore, the total fee sum for the group enough to cover the fee of both transactions.
+    """
+    result = (
+        algorand_client.new_group()
+        .add_transaction(transaction_a)
+        .add_transaction(transaction_b)
+        .send()
+    )
+
+    print(result.tx_ids)
+
+    # example: FEE_POOLING
+
+    # example: INNER_TXN_FEE
+
+    sp = algorand_client.get_suggested_params()
+
+    transaction_a = algorand_client.create_transaction.payment(
+        PaymentParams(
+            sender=account_a.address,
+            receiver=account_b.address,
+            amount=AlgoAmount(algo=1),
+        )
+    )
+
+    # TODO: add inner txn example
+
+    # example: INNER_TXN_FEE
 
 
 fees()
