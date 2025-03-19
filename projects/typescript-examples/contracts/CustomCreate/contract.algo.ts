b6adbf6c5a4dbb4ce3b42274888a80c47423110f
Chris Kim (Hyunggun) <chris.kim@algorand.foundation>
2025-03-19 12:34:52 -0700

    feat: add ts examples, custom create contract, remove unexpected artifact changes

---
+++ projects/typescript-examples/contracts/CustomCreate/contract.algo.ts
@@ -0,0 +1,15 @@
+import { Contract, Uint64, GlobalState, arc4 } from '@algorandfoundation/algorand-typescript'
+import type { uint64 } from '@algorandfoundation/algorand-typescript'
+
+export default class CustomCreate extends Contract {
+  public age = GlobalState<uint64>() // UInt64 with no default value
+
+  @arc4.abimethod({ onCreate: 'require' })
+  public custom_create(age: uint64): void {
+    this.age.value = Uint64(age)
+  }
+
+  public getAge(): uint64 {
+    return this.age.value
+  }
+}
